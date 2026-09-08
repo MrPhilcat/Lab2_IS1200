03d46d0dc403c2f3397681ae5515379ea7489d90
MrPhilcat <stah.philipp@gmail.com>
2026-09-08 15:43:34 +0200

    The cooler sieves added, still need regular

---
+++ sieves.c
@@ -0,0 +1,70 @@
+/*
+ print-primes.c
+ By Philipp Stah and Aron Corestav.
+ Last modified: 2026-09-08
+ This file is in the public domain.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define COLUMNS 6
+int counter = 0;
+
+
+void print_number(int n){
+  if (counter == COLUMNS){
+    printf("\n");
+    counter = 1;
+  }
+  else{
+    counter++;
+  }
+  printf("%10d ", n);
+}
+
+
+void print_sieves_cool(int n){
+    int myArray[n+1];
+    myArray[0] = 1;
+    myArray[1] = 1;
+    for(int i = 2; i <= n; i++){
+        myArray[i] = 0;
+    }
+
+    while (1){
+        int lastPrime = 0;
+        int j = 2;
+        while (lastPrime == 0){
+            if(myArray[j] == 0)
+                lastPrime = j;
+            if(j > n){
+                return;
+            }
+            j++;
+        }
+        print_number(lastPrime);
+    
+        for(int i = lastPrime; i <= n; i += lastPrime){
+            myArray[i] = 1;
+        }
+    }
+    
+}
+
+// 'argc' contains the number of program arguments, and
+// 'argv' is an array of char pointers, where each
+// char pointer points to a null-terminated string.
+int main(int argc, char *argv[]){
+    if(argc == 2)
+    {
+        print_sieves(atoi(argv[1]));
+    }
+  else
+    printf("Please state an integer number.\n");
+  return 0;
+}
+
+ 
