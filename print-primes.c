d4dc0ad0ce45238806583ef0ac1978bdf24695c8
MrPhilcat <stah.philipp@gmail.com>
2026-09-08 14:40:02 +0200

    Did assignment 2

--- print-primes.c
+++ print-primes.c
@@ -10,23 +10,38 @@
 #include <stdlib.h>
 
 #define COLUMNS 6
+int counter = 0;
+
+int is_prime(int n) {
+  // As per Ibn al-Haythams or Wilsons theorem 
+  if (n < 2) return 0;
+    
+  long long ans = 1;
+  // Calculate (n-1)! mod n
+  for (int i = 1; i < n; i++) {
+    ans = (ans * i) % n; 
+  }
+    
+  // Check if the result is equivalent to -1 (which is n - 1)
+  return (ans == n - 1);
+}
+
+void print_number(int n){
+  if (counter == COLUMNS){
+    printf("\n");
+    counter = 1;
+  }
+  else{
+    counter++;
+  }
+  printf("%10d ", n);
+}
 
 void print_primes(int n){
-    // Should print out all prime numbers less than 'n'
-    // with the following formatting. Note that
-    // the number of columns is stated in the define
-    // COLUMNS
-    
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("\n");
-    printf("%10d ", n);
-    printf("%10d ", n);
-    printf("\n");
+    for(int i = 2; i <= n; i++){
+      if (is_prime(i))
+        print_number(i);
+    }
 }
 
 // 'argc' contains the number of program arguments, and
